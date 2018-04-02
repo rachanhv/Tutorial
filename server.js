da4475c5d7a5957ac0d1f22106a7a00afc9f71b8
rachangowda <rachanr7@gmial.com>
2018-04-02 14:46:59 +0530

    added index.hmtl

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//port
+var port = process.env.PORT || 8080;
+
+
+app.listen(port, () => console.log('Example app listening on port 3000!'))
